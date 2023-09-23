8719431c712bffad21dcc0cf1fa040d423515ae8
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-09-23 05:56:51 -0400

    Add to netlify.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 /*
   Components
@@ -26,11 +27,21 @@ function App() {
 
   return (
     <div className="wrapper">
-      <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
-      <Footer />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home employees={employees} owners={owners} pets={pets} />}
+          />
+          <Route
+            path="/staffList"
+            element={<StaffList employees={employees} />}
+          />
+          <Route path="/petsList" element={<PetsList pets={pets} />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
